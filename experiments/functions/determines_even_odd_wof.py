98a4af22cc55b6dc37b2fd14ad3a2ea8b2268261
AugustineAniobasi <augustineaniobasi@gmail.com>
2026-03-24 12:35:11 +0000

    feat: Add even_odd script, hello script, and compiled bytecode

---
+++ experiments/functions/determines_even_odd_wof.py
@@ -0,0 +1,37 @@
+
+
+numbers = [22, 45, 47, 49, 54] # This is a list in python that contains a list of numbers
+
+# numbers[i] means the element in the ith index, for the first element i = 0
+
+if numbers[0] % 2 == 0: 
+  print(f"The number {numbers[0]} is even")
+else:
+  print(f"The number {numbers[0]} is odd")
+
+  
+if numbers[1] % 2 == 0:
+  print(f"The number {numbers[1]} is even")
+else:
+  print(f"The number {numbers[1]} is odd")
+
+ 
+if numbers[2] % 2 == 0:
+  print(f"The number {numbers[2]} is even")
+else:
+  print(f"The number {numbers[2]} is odd")
+
+  
+if numbers[3] % 2 == 0:
+  print(f"The number {numbers[3]} is even")
+else:
+  print(f"The number {numbers[3]} is odd")
+
+
+if numbers[3] % 2 == 0:
+  print(f"The number {numbers[3]} is even")
+else:
+  print(f"The number {numbers[3]} is odd")
+
+
+ 
---
+++ notes/How_Python_executes.md
@@ -0,0 +1,3 @@
+# Python compiler or Interpreter
+
+Python is an hybrid language that utilizes both a compiler and interpreter to execute python codes. it works by compiling the python code into bytecodes with the aid of a compiler and then further more through a python virtual machine uses an interpretr to translates the bytecodes to machine language for the computer system to execute.
